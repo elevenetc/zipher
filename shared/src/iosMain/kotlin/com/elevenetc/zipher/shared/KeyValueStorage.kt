44baed6bd67a011bf961136f986041f0270ae304
elevenetc <shumvlesu@gmail.com>
2021-03-08 02:04:58 +0100

    Add iOS KevValueStorage implementation

--- shared/src/iosMain/kotlin/com/elevenetc/zipher/shared/KeyValueStorage.kt
+++ shared/src/iosMain/kotlin/com/elevenetc/zipher/shared/KeyValueStorage.kt
@@ -1,10 +1,24 @@
 package com.elevenetc.zipher.shared
 
+import platform.Foundation.NSUserDefaults
+
 actual class KeyValueStorage {
+
+    private val defaults = NSUserDefaults.standardUserDefaults()
+
     actual fun store(key: String, value: String) {
+        defaults.setObject(value, key)
     }
 
     actual fun get(key: String, defaultValue: String): String {
-        TODO("Not yet implemented")
+        return defaults.stringForKey(key) ?: defaultValue
+    }
+
+    actual fun store(key: String, value: Boolean) {
+        defaults.setBool(value, key)
+    }
+
+    actual fun get(key: String, defaultValue: Boolean): Boolean {
+        return defaults.boolForKey(key)
     }
 }
